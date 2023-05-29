c0b3967563807cb11809f08a2b0b56ae21c7207b
H1rono <hronok66@gmail.com>
2023-05-29 19:42:53 +0900

    :recycle: Refactor `blink`
    export initialization into a function

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -22,36 +22,55 @@ use bsp::hal::{
     watchdog::Watchdog,
 };
 
+struct Machine {
+    pub delay: cortex_m::delay::Delay,
+    pub pins: bsp::Pins,
+}
+
+impl Machine {
+    fn take() -> Option<Self> {
+        static mut TAKEN: bool = false;
+        if unsafe { TAKEN } {
+            return None;
+        }
+        unsafe {
+            TAKEN = true;
+        }
+        let mut pac = pac::Peripherals::take().unwrap();
+        let core = pac::CorePeripherals::take().unwrap();
+        let mut watchdog = Watchdog::new(pac.WATCHDOG);
+        let sio = Sio::new(pac.SIO);
+
+        // External high-speed crystal on the pico board is 12Mhz
+        let external_xtal_freq_hz = 12_000_000u32;
+        let clocks = init_clocks_and_plls(
+            external_xtal_freq_hz,
+            pac.XOSC,
+            pac.CLOCKS,
+            pac.PLL_SYS,
+            pac.PLL_USB,
+            &mut pac.RESETS,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+        let pins = bsp::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+        Some(Self { delay, pins })
+    }
+}
+
 #[entry]
 fn main() -> ! {
     info!("Program start");
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
-
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+    let Machine { mut delay, pins } = Machine::take().unwrap();
 
     // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
     // on-board LED, it might need to be changed.
