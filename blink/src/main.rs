ce1758c2ad370289be2af3f5d4f0ca1a16d708c6
H1rono <hronok66@gmail.com>
2023-05-30 03:19:06 +0900

    :fire: Remove comments in `blink`

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -1,6 +1,3 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
@@ -15,10 +12,7 @@ use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
@@ -41,7 +35,6 @@ static MACHINE: Lazy<Mutex<RefCell<Machine>>> = Lazy::new(|| {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
@@ -64,11 +57,6 @@ static MACHINE: Lazy<Mutex<RefCell<Machine>>> = Lazy::new(|| {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
     let led_pin = pins.led.into_push_pull_output();
     Mutex::new(RefCell::new(Machine { delay, led_pin }))
 });
@@ -97,5 +85,3 @@ fn main() -> ! {
         delay.delay_ms(500);
     }
 }
-
-// End of file
