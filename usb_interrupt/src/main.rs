e8d2fcf729af8e3676006862139254e83b2141fa
H1rono <hronok66@gmail.com>
2023-06-22 12:18:52 +0900

    :recycle: Less `unsafe` in `usb_interrupt`

--- usb_interrupt/src/main.rs
+++ usb_interrupt/src/main.rs
@@ -1,63 +1,74 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+
 use defmt::info;
 use defmt_rtt as _;
 use panic_probe as _;
 
 use bsp::entry;
+use bsp::hal;
 use rp_pico as bsp;
 
-use bsp::hal::{
-    self,
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-};
+// Peripherals
+use bsp::hal::{pac, sio::Sio, watchdog::Watchdog};
+
+// Clocks
+use bsp::hal::clocks::{init_clocks_and_plls, Clock};
+use cortex_m::delay::Delay;
+
+// Concurrency
+use core::cell::RefCell;
+use cortex_m::interrupt::{free, Mutex};
+use once_cell::sync::Lazy;
+
+// memory allocator
+use embedded_alloc::Heap;
 
 // GPIO
+use bsp::hal::gpio::{bank0::Gpio25, Pin, PinId, PushPullOutput};
 use embedded_hal::digital::v2::OutputPin;
 
 // interrupt macro
 use bsp::hal::pac::interrupt;
 
 // USB Device support
+use hal::usb::UsbBus;
 use usb_device::{
     class_prelude::UsbBusAllocator,
     prelude::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
 };
-
-// USB Communications Class Device support
 use usbd_serial::SerialPort;
 
-/// The USB Device Driver (shared with the interrupt).
-static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+// To construct self-referential struct
+use self_cell::self_cell;
+type UsbOwner = UsbBusAllocator<UsbBus>;
+struct UsbDependent<'a> {
+    device: UsbDevice<'a, UsbBus>,
+    serial: SerialPort<'a, UsbBus>,
+}
+self_cell!(
+    struct Usb {
+        owner: UsbOwner,
+        #[covariant]
+        dependent: UsbDependent,
+    }
+);
 
-/// The USB Bus Driver (shared with the interrupt).
-static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+struct Machine<I: PinId> {
+    delay: Option<Delay>,
+    led: Pin<I, PushPullOutput>,
+    usb: Usb,
+}
 
-/// The USB Serial Device Driver (shared with the interrupt).
-static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
 
-/// Entry point to our bare-metal application.
-///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
-#[entry]
-fn main() -> ! {
-    info!("Program start");
-    // Grab our singleton objects
+static MACHINE: Lazy<Mutex<RefCell<Machine<Gpio25>>>> = Lazy::new(|| {
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-
-    // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-
-    // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let clocks = init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
@@ -70,105 +81,96 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // Set up the USB driver
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USBCTRL_REGS,
-        pac.USBCTRL_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_BUS = Some(usb_bus);
-    }
-
-    // Grab a reference to the USB Bus allocator. We are promising to the
-    // compiler not to take mutable access to this global variable whilst this
-    // reference exists!
-    let bus_ref = unsafe { USB_BUS.as_ref().unwrap() };
-
-    // Set up the USB Communications Class Device driver
-    let serial = SerialPort::new(bus_ref);
-    unsafe {
-        USB_SERIAL = Some(serial);
-    }
-
-    // Create a USB device with a fake VID and PID
-    let usb_dev = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x16c0, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
-        .device_class(2) // from: https://www.usb.org/defined-class-codes
-        .build();
-    unsafe {
-        // Note (safety): This is safe as interrupts haven't been started yet
-        USB_DEVICE = Some(usb_dev);
-    }
-
-    // Enable the USB interrupt
-    unsafe {
-        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
-    };
-
-    // No more USB code after this point in main! We can do anything we want in
-    // here since USB is handled in the interrupt - let's blink an LED!
-
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
-
-    // Set the pins up according to their function on this particular board
+    let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let sio = Sio::new(pac.SIO);
     let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+    let led_pin = pins.led.into_push_pull_output();
 
-    // Set the LED to be an output
-    let mut led_pin = pins.led.into_push_pull_output();
+    let usb_bus = UsbBusAllocator::new(UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    Mutex::new(RefCell::new(Machine {
+        delay: Some(delay),
+        led: led_pin,
+        usb: Usb::new(usb_bus, |bus| UsbDependent {
+            device: UsbDeviceBuilder::new(bus, UsbVidPid(0x16c0, 0x27dd))
+                .manufacturer("Fake company")
+                .product("Serial port")
+                .serial_number("TEST")
+                .device_class(2) // from: https://www.usb.org/defined-class-codes
+                .build(),
+            serial: SerialPort::new(bus),
+        }),
+    }))
+});
+
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
+    // take `delay` from the machine
+    let mut delay = free(|cs| MACHINE.borrow(cs).borrow_mut().delay.take()).unwrap();
+    // Enable the USB interrupt
+    unsafe {
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
 
     // Blink the LED at 1 Hz
     loop {
-        led_pin.set_high().unwrap();
+        free(|cs| MACHINE.borrow(cs).borrow_mut().led.set_high().unwrap());
         delay.delay_ms(500);
-        led_pin.set_low().unwrap();
+        free(|cs| MACHINE.borrow(cs).borrow_mut().led.set_high().unwrap());
         delay.delay_ms(500);
     }
 }
 
-/// This function is called whenever the USB Hardware generates an Interrupt
-/// Request.
-///
-/// We do all our USB work under interrupt, so the main thread can continue on
-/// knowing nothing about USB.
 #[allow(non_snake_case)]
 #[interrupt]
 unsafe fn USBCTRL_IRQ() {
     info!("interrupt");
+    free(|cs| {
+        MACHINE
+            .borrow(cs)
+            .borrow_mut()
+            .usb
+            .with_dependent_mut(|_, dep| usbctrl_irq(dep));
+    });
+}
+
+fn usbctrl_irq(dep: &mut UsbDependent) {
     use core::sync::atomic::{AtomicBool, Ordering};
 
     /// Note whether we've already printed the "hello" message.
     static SAID_HELLO: AtomicBool = AtomicBool::new(false);
 
-    // Grab the global objects. This is OK as we only access them under interrupt.
-    let usb_dev = USB_DEVICE.as_mut().unwrap();
-    let serial = USB_SERIAL.as_mut().unwrap();
-
     // Say hello exactly once on start-up
     if !SAID_HELLO.load(Ordering::Relaxed) {
         SAID_HELLO.store(true, Ordering::Relaxed);
-        let _ = serial.write(b"Hello, World!\r\n");
+        let _ = dep.serial.write(b"Hello, World!\r\n");
     }
 
     // Poll the USB driver with all of our supported USB Classes
-    if usb_dev.poll(&mut [serial]) {
+    if dep.device.poll(&mut [&mut dep.serial]) {
         let mut buf = [0u8; 64];
-        match serial.read(&mut buf) {
+        match dep.serial.read(&mut buf) {
             Err(_e) => {
                 // Do nothing
             }
@@ -184,7 +186,7 @@ unsafe fn USBCTRL_IRQ() {
                 // Send back to the host
                 let mut wr_ptr = &buf[..count];
                 while !wr_ptr.is_empty() {
-                    let _ = serial.write(wr_ptr).map(|len| {
+                    let _ = dep.serial.write(wr_ptr).map(|len| {
                         wr_ptr = &wr_ptr[len..];
                     });
                 }
