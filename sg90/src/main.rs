b89b31c9c1964c07baf62fb881da8824cc16cba7
H1rono <hronok66@gmail.com>
2023-07-19 15:09:21 +0900

    :recycle: Use bsp::XOSC_CRYSTAL_FREQ

--- sg90/src/main.rs
+++ sg90/src/main.rs
@@ -12,7 +12,7 @@ use panic_probe as _;
 use rp_pico as bsp;
 
 // Entry point
-use bsp::entry;
+use bsp::{entry, XOSC_CRYSTAL_FREQ};
 
 // Peripherals
 use bsp::hal::{pac, sio::Sio, watchdog::Watchdog};
@@ -45,9 +45,8 @@ static MACHINE: Lazy<Mutex<RefCell<Machine>>> = Lazy::new(|| {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+        XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
